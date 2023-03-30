a842a84f956891d5fdca1f77d7cdb0b1fe8b6a4c
Aaron Arredondo <10608950-aaron.arredondo@users.noreply.gitlab.com>
2023-03-30 14:28:56 -0600

    style: 💅 formatting

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -3,37 +3,37 @@ M.config = {
     active = false,
     setup = {
         plugins = {
-            marks = true, -- shows a list of your marks on ' and `
+            marks = true,     -- shows a list of your marks on ' and `
             registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
             -- the presets plugin, adds help for a bunch of default keybindings in Neovim
             -- No actual key bindings are created
             presets = {
-                operators = true, -- adds help for operators like d, y, ...
-                motions = true, -- adds help for motions
-                text_objects = true, -- help for text objects triggered after entering an operator
-                windows = true, -- default bindings on <c-w>
-                nav = true, -- misc bindings to work with windows
-                z = true, -- bindings for folds, spelling and others prefixed with z
-                g = true, -- bindings for prefixed with g
+                operators = true,                            -- adds help for operators like d, y, ...
+                motions = true,                              -- adds help for motions
+                text_objects = true,                         -- help for text objects triggered after entering an operator
+                windows = true,                              -- default bindings on <c-w>
+                nav = true,                                  -- misc bindings to work with windows
+                z = true,                                    -- bindings for folds, spelling and others prefixed with z
+                g = true,                                    -- bindings for prefixed with g
             },
             spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
         },
         icons = {
             breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
             separator = "➜", -- symbol used between a key and it's label
-            group = "+", -- symbol prepended to a group
+            group = "+",      -- symbol prepended to a group
         },
         window = {
-            border = "none", -- none, single, double, shadow
-            position = "bottom", -- bottom, top
-            margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+            border = "none",          -- none, single, double, shadow
+            position = "bottom",      -- bottom, top
+            margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
             padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
             -- winblend = 0,
         },
         layout = {
             height = { min = 4, max = 25 }, -- min and max height of the columns
             width = { min = 20, max = 50 }, -- min and max width of the columns
-            spacing = 3, -- spacing between columns
+            spacing = 3,                    -- spacing between columns
             -- align = "center",
         },
         hidden = {
@@ -45,41 +45,40 @@ M.config = {
             "lua",
             "^:",
             "^ ",
-        }, -- hide mapping boilerplate
+        },                -- hide mapping boilerplate
         show_help = true, -- show help message on the command line when the popup is visible
     },
-
     opts = {
-        mode = "n", -- NORMAL mode
+        mode = "n",     -- NORMAL mode
         prefix = "<leader>",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     },
     vopts = {
-        mode = "v", -- VISUAL mode
+        mode = "v",     -- VISUAL mode
         prefix = "<leader>",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     },
     secopts = {
-        mode = "n", -- NORMAL mode
+        mode = "n",     -- NORMAL mode
         prefix = "\\",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     },
     secvopts = {
-        mode = "v", -- VISUAL mode
+        mode = "v",     -- VISUAL mode
         prefix = "\\",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
     },
     -- NOTE: Prefer using : over <cmd> as the latter avoids going back in normal-mode.
     -- see https://neovim.io/doc/user/map.html#:map-cmd
