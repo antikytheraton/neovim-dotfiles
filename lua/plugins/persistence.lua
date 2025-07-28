416eb3d7314fde76efb3b3c2ed37a6f370d2b014
Aaron Arredondo <137956493+motic-guy@users.noreply.github.com>
2025-07-28 12:12:35 -0600

    fix(persistence): save winpos and other buffer things

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -7,5 +7,6 @@ return {
   },
   opts = {
     dir = vim.fn.expand(vim.fn.stdpath("config") .. "/.session/"),
+    options = { "blank", "buffers", "curdir", "folds", "help", "tabpages", "winsize", "winpos" }
   }
 }
