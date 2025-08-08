32ee149aac9f3ee8f6bb707d853d8cac1af64b1d
Aaron Arredondo <16123078+antikytheraton@users.noreply.github.com>
2025-08-08 16:02:44 -0600

    clean code

--- lua/config/efm/prettierd.lua
+++
@@ -1,7 +0,0 @@
-return {
-    formatCommand = "prettier --stdin-filepath ${INPUT}",
-    formatStdin = true,
-    env = {
-        string.format("PRETTIERD_DEFAULT_CONFIG=%s", vim.fn.expand("~/.config/nvim/lua/config/efm/.prettierrc.json")),
-    },
-}
