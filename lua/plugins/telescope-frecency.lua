deef81fb2626f52ffea89a340b2e96cb4fe73c4c
Aaron Arredondo <137956493+motic-guy@users.noreply.github.com>
2025-11-06 12:54:28 -0600

    fix: enable fuzzy finder for frecency plugin 🔭

--- lua/plugins/telescope-frecency.lua
+++ lua/plugins/telescope-frecency.lua
@@ -1,12 +1,16 @@
 return {
-  -- https://github.com/nvim-telescope/telescope-frecency.nvim
-  "nvim-telescope/telescope-frecency.nvim",
-  -- install the latest stable version
-  version = "*",
-  config = function(_, opts)
-    require("telescope-frecency").setup({
-      db_safe_mode = false
-    })
-    require("telescope").load_extension("frecency")
-  end,
+	-- https://github.com/nvim-telescope/telescope-frecency.nvim
+	"nvim-telescope/telescope-frecency.nvim",
+	-- install the latest stable version
+	version = "*",
+	config = function(_, opts)
+		require("telescope-frecency").setup({
+			db_safe_mode = false,
+			matcher = "fuzzy",
+			scoring_function = function(recency, fzy_score)
+				return -recency
+			end,
+		})
+		require("telescope").load_extension("frecency")
+	end,
 }
