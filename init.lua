2bf839bd4d9449027e0983aaea034c05c089c9cb
antikytheraton <binauralvoice@gmail.com>
2026-06-19 21:02:59 -0600

    fix: adjust nixos config

--- init.lua
+++ init.lua
@@ -21,6 +21,7 @@ require("lazy").setup("plugins", {
     enabled = true, -- automatically check for config file changes and reload the ui
     notify = false, -- turn off notifications whenever plugin changes are made
   },
+  lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json", -- moves it to ~/.local/share/nvim/
 })
 
 -- These modules are not loaded by lazy
